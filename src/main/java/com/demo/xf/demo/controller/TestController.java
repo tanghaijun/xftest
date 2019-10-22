13123d6d37e781c75953be0eba71bcafc35a4189
Tanghaijun <tanghaijun@fang.com>
2019-10-22 15:00:04 +0800

    xx

---
+++ src/main/java/com/demo/xf/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.demo.xf.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return new Date().toString();
+    }
+}
